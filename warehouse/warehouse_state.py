58f6a1ebc8c8e09536278f33fc7cd205849dca12
Rúben Garcia <rube200@users.noreply.github.com>
2023-06-22 21:31:46 +0100

    Change state to just handle one forklift and colisions

--- warehouse/warehouse_state.py
+++ warehouse/warehouse_state.py
@@ -12,123 +12,86 @@ class WarehouseState(State[Action]):
 
     def __init__(self, matrix: ndarray, rows, columns):
         super().__init__()
-        self.steps = 0
 
         self.rows = rows
         self.columns = columns
-        self.current_forklift = -1
-        self.forklifts = []
         self.matrix = np.full([self.rows, self.columns], fill_value=0, dtype=int)
 
         for i in range(self.rows):
             for j in range(self.columns):
                 self.matrix[i][j] = matrix[i][j]
                 if self.matrix[i][j] == constants.FORKLIFT:
-                    self.forklifts.append(Cell(i, j))
-                    self.current_forklift = len(self.forklifts) - 1
+                    self.line_forklift = i
+                    self.column_forklift = j
                 elif self.matrix[i][j] == constants.EXIT:
                     self.line_exit = i
                     self.column_exit = j
 
-    def choose_forklift_by_cell(self, cell: Cell) -> bool:
-        forklift_index = self.get_forklift_index_by_cell(cell)
-        if forklift_index == -1:
-            return False
-
-        self.current_forklift = forklift_index
-        return True
-
-    def get_forklift_index_by_cell(self, cell: Cell, ignore_index: int = -1) -> int:
-        for i in range(len(self.forklifts)):
-            if i == ignore_index:
-                continue
-
-            forklift = self.forklifts[i]
-            if forklift.line == cell.line and forklift.column == cell.column:
-                return i
-
-        return -1
-
-    def get_current_forklift(self) -> (int, int):
-        forklift = self.forklifts[self.current_forklift]
-        return forklift.line, forklift.column
-
     @staticmethod
     def is_movable_cell(target_cell: Cell) -> bool:
-        return target_cell == constants.EMPTY or target_cell == constants.EXIT or target_cell == constants.FORKLIFT
+        return target_cell == constants.EMPTY or target_cell == constants.EXIT
+
+    @staticmethod
+    def can_not_move() -> bool:
+        return True
 
     def can_move_up(self) -> bool:
-        line_forklift, column_forklift = self.get_current_forklift()
-        if line_forklift <= 0:
+        if self.line_forklift <= 0:
             return False
 
-        target_cell = self.matrix[line_forklift - 1][column_forklift]
+        target_cell = self.matrix[self.line_forklift - 1][self.column_forklift]
         return self.is_movable_cell(target_cell)
 
     def can_move_right(self) -> bool:
-        line_forklift, column_forklift = self.get_current_forklift()
-        if column_forklift == self.columns - 1:
+        if self.column_forklift == self.columns - 1:
             return False
 
-        target_cell = self.matrix[line_forklift][column_forklift + 1]
+        target_cell = self.matrix[self.line_forklift][self.column_forklift + 1]
         return self.is_movable_cell(target_cell)
 
     def can_move_down(self) -> bool:
-        line_forklift, column_forklift = self.get_current_forklift()
-        if line_forklift == self.rows - 1:
+        if self.line_forklift == self.rows - 1:
             return False
 
-        target_cell = self.matrix[line_forklift + 1][column_forklift]
+        target_cell = self.matrix[self.line_forklift + 1][self.column_forklift]
         return self.is_movable_cell(target_cell)
 
     def can_move_left(self) -> bool:
-        line_forklift, column_forklift = self.get_current_forklift()
-        if column_forklift <= 0:
+        if self.column_forklift <= 0:
             return False
 
-        target_cell = self.matrix[line_forklift][column_forklift - 1]
+        target_cell = self.matrix[self.line_forklift][self.column_forklift - 1]
         return self.is_movable_cell(target_cell)
 
-    def update_forklift_in_matrix(self, new_line: int, new_column: int, increment_steps: bool = False):  # todo colisoes
-        line_forklift, column_forklift = self.get_current_forklift()
-        if line_forklift != self.line_exit or column_forklift != self.column_exit:
-            another_forklift_index = self.get_forklift_index_by_cell(Cell(line_forklift, column_forklift), self.current_forklift)
-            if another_forklift_index != -1:
-                old_cell_state = constants.FORKLIFT
-            else:
-                old_cell_state = constants.EMPTY
+    def update_forklift_in_matrix(self, new_line: int, new_column: int):
+        if self.line_forklift != self.line_exit or self.column_forklift != self.column_exit:
+            old_cell_state = constants.EMPTY
         else:
             old_cell_state = constants.EXIT
 
-        self.matrix[line_forklift][column_forklift] = old_cell_state
-        self.forklifts[self.current_forklift] = Cell(new_line, new_column)
+        self.matrix[self.line_forklift][self.column_forklift] = old_cell_state
+        self.line_forklift = new_line
+        self.column_forklift = new_column
         self.matrix[new_line][new_column] = constants.FORKLIFT
 
-        if increment_steps:
-            self.steps += 1
+    def not_move(self) -> None:
+        pass
 
     def move_up(self) -> None:
-        line_forklift, column_forklift = self.get_current_forklift()
-        self.update_forklift_in_matrix(line_forklift - 1, column_forklift, True)
+        self.update_forklift_in_matrix(self.line_forklift - 1, self.column_forklift)
 
     def move_right(self) -> None:
-        line_forklift, column_forklift = self.get_current_forklift()
-        self.update_forklift_in_matrix(line_forklift, column_forklift + 1, True)
+        self.update_forklift_in_matrix(self.line_forklift, self.column_forklift + 1)
 
     def move_down(self) -> None:
-        line_forklift, column_forklift = self.get_current_forklift()
-        self.update_forklift_in_matrix(line_forklift + 1, column_forklift, True)
+        self.update_forklift_in_matrix(self.line_forklift + 1, self.column_forklift)
 
     def move_left(self) -> None:
-        line_forklift, column_forklift = self.get_current_forklift()
-        self.update_forklift_in_matrix(line_forklift, column_forklift - 1, True)
+        self.update_forklift_in_matrix(self.line_forklift, self.column_forklift - 1)
 
     def get_cell_color(self, row: int, column: int) -> Color:
-        if row == self.line_exit and column == self.column_exit:
-            for forklift in self.forklifts:
-                if forklift.line == row and forklift.column == column:
-                    return constants.COLORFORKLIFT
-
+        if row == self.line_exit and column == self.column_exit and (
+                row != self.line_forklift or column != self.column_forklift):
             return constants.COLOREXIT
 
         if self.matrix[row][column] == constants.PRODUCT_CATCH:
