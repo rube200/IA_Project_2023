459fc73c910625ed5dfb82e35535ac4c071d35bf
Rúben Garcia <rube200@users.noreply.github.com>
2023-06-23 02:41:18 +0100

    Inversible pair and str

--- warehouse/pair.py
+++ warehouse/pair.py
@@ -1,3 +1,4 @@
+from warehouse.actions import *
 from warehouse.cell import Cell
 
 
@@ -13,4 +14,32 @@ class Pair:
             self.cell2.line) + "_" + str(self.cell2.column)
 
     def __str__(self):
-        return f'{self.cell1.line}-{self.cell1.column} / {self.cell2.line}-{self.cell2.column}: {self.value} -> {[str(x) for x in self.actions]}\n'
+        string = f'{self.cell1.line}-{self.cell1.column} / {self.cell2.line}-{self.cell2.column}: {self.value}->'
+
+        num_actions = len(self.actions)
+        for a in range(num_actions):
+            action = self.actions[a]
+            string += f'{action}'
+            if a + 1 < num_actions:
+                string += ','
+
+        string += '\n'
+        return string
+
+    def __invert__(self):
+        new_pair = Pair(self.cell2, self.cell1)
+
+        for action in self.actions[::-1]:
+            if type(action) == ActionUp:
+                new_pair.actions.append(ActionDown())
+            elif type(action) == ActionDown:
+                new_pair.actions.append(ActionUp())
+            elif type(action) == ActionLeft:
+                new_pair.actions.append(ActionRight())
+            elif type(action) == ActionRight:
+                new_pair.actions.append(ActionLeft())
+            else:
+                new_pair.actions.append(ActionNotMove)
+
+        new_pair.value = self.value
+        return new_pair
